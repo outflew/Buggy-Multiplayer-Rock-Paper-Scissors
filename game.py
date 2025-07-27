fa32f4dc73851fad2346ba200bbe220d38b2894a
outflew <aliahmedadelgad@gmail.com>
2025-07-27 13:49:56 +0300

    fixed elif by removing /

--- game.py
+++ game.py
@@ -5,8 +5,8 @@ def get_player_choice(player_num):
 def determine_winner(player1_choice, player2_choice):
     if player1_choice == player2_choice  #
         return "It's a tie!"
-    elif (player1_choice == "rock" and player2_choice == "scissors") or \  # 
-         (player1_choice == "paper" and player2_choice == "rock") or \
+    elif (player1_choice == "rock" and player2_choice == "scissors") or   # 
+         (player1_choice == "paper" and player2_choice == "rock") or
          (player1_choice == "scissors" and player2_choice == "paper"):
         return "Player 1 wins!"
     else:
